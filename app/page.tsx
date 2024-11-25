41544c6bc68f3fd14b1af9e46f7200e35145f129
kilianmarkgraf <legojovo@gmx.de>
2024-11-25 20:33:54 +0100

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,124 @@
-import Image from "next/image";
+"use client";
+
+import { useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [stage, setStage] = useState(0);
+  const [error, setError] = useState<string | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const login = () => {
+    if (usernameRef.current?.value && passwordRef.current?.value) {
+      if (
+        usernameRef.current.value == "Klausi" &&
+        passwordRef.current.value == "#Februar25"
+      ) {
+        setStage(1);
+        setError(null);
+      } else {
+        setError("Faaalsch!");
+      }
+    }
+  };
+
+  const checkMail = () => {
+    setStage(2);
+  };
+  return (
+    <div className="w-full h-screen bg-red-700 p-6">
+      <main className="flex-col">
+        {stage == 2 && (
+          <>
+            <>
+              <p>Sender: {"kilian.markgraf@gmx.de"}</p>
+              <p>Betreff: Luisa Geschenk Gewinnkombination</p>
+
+              <p className="mt-4">
+                Lieber Weihnachtsmann, wie geht es dir? Ich hoffe wir können
+                demnächst mal wieder zusammen ein bisschen Schlitten reiten
+                zusammen ne Milch mit Keksen oder so essen.
+              </p>
+              <p className="mt-2">
+                Kommen wir zum wichtigen: Luisa ist fleißig am rätseln und wird zu Weihnachten
+                voraussichtlich alle Rätsel ihres Kalenders gelöst haben. Bitte
+                gehe mit ihr die Lösungen zusammen durch, damit sie das
+                Lösungswort bestimmen kann. Ich habe es dir ja erklärt. Du weißt
+                schon, das mit dem Kartendeck in meinem Kilian-Fach... Danke schonmal im Vorraus!!!
+              </p>
+              <p className="mt-2">Liebe Grüße, Kilian</p>
+            </>
+          </>
+        )}
+        {stage == 1 && (
+          <>
+            <>
+              <h1 className="text-2xl">
+                Hallo Weihnachtsmann, hier sind deine letzten Mails:
+              </h1>
+
+              <div>
+                <p className="mt-4 underline">
+                  Rudolf das Rentier: {"Krankheit meines Kindes"}
+                </p>
+                <p className="mt-4 underline">
+                  Elf 3:{" "}
+                  {"Bestellung fertig Margarete Dix, bereit zur Austragung"}
+                </p>
+                <p className="mt-4 underline">
+                  Rudolf das Rentier: {"Krankheit meines Kindes"}
+                </p>
+                <p className="mt-4 underline">
+                  Elf 10:{" "}
+                  {"Bestellung fertig Felina Markgraf, bereit zur Austragung"}
+                </p>
+                <p className="mt-4 underline">
+                  Elf 2:{" "}
+                  {"Bestellung fertig Julian Gippner, bereit zur Austragung"}
+                </p>
+                <p className="mt-4 underline" onClick={checkMail}>
+                  Kilian Markgraf: {"Luisa Geschenk Gewinnkombination"}
+                </p>
+                <p className="mt-4 underline">
+                  Elf 6:{" "}
+                  {"Bestellung fertig Paula Wegner, bereit zur Austragung"}
+                </p>
+              </div>
+            </>
+          </>
+        )}
+        {stage == 0 && (
+          <>
+            <h1 className="text-4xl">Weihnachtsmann Admin Panel</h1>
+            <p className="text-2xl mt-6">
+              Hallo Weihnachtsmann, bist du ess wirklich?
+            </p>
+            <div>
+              <p>Benutzername:</p>
+              <input
+                ref={usernameRef}
+                className="bg-white rounded-md text-black min-w-60 w-full h-10 text-2xl"
+                type="text"
+              />
+            </div>
+            <div>
+              <p>Passwort:</p>
+              <input
+                ref={passwordRef}
+                className="bg-white rounded-md text-black min-w-60 w-full h-10 text-2xl"
+                type="text"
+              />
+            </div>
+            <button
+              className="bg-white rounded-md text-red-700 min-w-60 mt-8 h-10 w-full text-2xl"
+              onClick={login}
+            >
+              Login
+            </button>
+          </>
+        )}
+        {error && <p className="text-2xl">{error}</p>}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
